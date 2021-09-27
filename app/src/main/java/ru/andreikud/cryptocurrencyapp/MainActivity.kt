6ba8c20b6cc386b92b208978df444af17beb0ad6
Andrei <andreikudrya1995@gmail.com>
2021-09-27 20:34:49 +0500

    Dependencies. Compose setup.

--- app/src/main/java/ru/andreikud/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/ru/andreikud/cryptocurrencyapp/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.andreikud.cryptocurrencyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import ru.andreikud.cryptocurrencyapp.ui.theme.CryptocurrencyAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            CryptocurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
     }
 }
